aa66ec81780973448f8279b855a03c86fd479828
bae jiun, maybe <alice.maydev@gmail.com>
2017-10-01 23:53:33 +0900

    Update original
    Former-commit-id: b28d16971f0bf171c926aed8b2241ee020dd5864

--- project1/src/main.cc
+++ project1/src/main.cc
@@ -6,48 +6,55 @@
 #include <algorithm>
 #include <iterator>
 
-#include <ahocorasick.h>
 
 #define newl ('\n')
 #define sep ('|')
-#define DEFAULT_RESERVE_SIZE 256
+#include <ahocorasick.h>
+
+using namespace std;
 
 int main(int argc, char * argv[]) {
     int n = 0;
     char cmd;
-    std::string query;
+    string query;
+    set<string> patterns;
+
     std::ios_base::sync_with_stdio(false);
     std::cin >> n;
-    query.reserve(DEFAULT_RESERVE_SIZE);
-    ahocorasick::Operator op;
     for (int i = 0; i < n; i++) {
         std::cin >> query;
-        op.insert(query);
+        patterns.insert(query);
     }
+    Table* table = new Table(patterns);
     std::cout << "R" << newl;
+
+
     while (std::cin >> cmd) {
         std::cin.get();
         getline(std::cin, query);
+
         switch (cmd) {
         case 'Q': {
-            bool flag = false;
-            if (!op.wrapper(op.match(query), [&flag](const std::string pattern) {
-                if (!flag) {
-                    std::cout << pattern;
-                    flag = true;
-                } else
-                    std::cout << sep << pattern;
-            })) {
+            list<string> matches = table->match(query);
+
+            auto begin = matches.begin();
+            if (begin != matches.end()) {
+                std::cout << *(begin++);
+                while (begin != matches.end()) {
+                    std::cout << sep;
+                    std::cout << *(begin++);
+                }
+            } else {
                 std::cout << -1;
             }
             std::cout << newl;
         }
-            break;
+                  break;
         case 'A':
-            op.insert(query);
+            table->add(query);
             break;
         case 'D':
-            op.erase(query);
+            table->remove(query);
             break;
         }
     }
