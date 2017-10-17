cf2dd9f9c62a8d239b15cff2c8f864595e3a18ce
bae jiun, maybe <alice.maydev@gmail.com>
2017-10-17 10:56:35 +0900

    start project2

---
+++ project2/include/argparser.hpp
@@ -0,0 +1,21 @@
+#include <string>
+
+namespace arg {
+    class Parser {
+    public:
+        bool argument(std::string name) {
+
+        }
+
+        template <typename T>
+        T& get(const string& name) {
+            
+        }
+
+        void parse(int argc, char * argv[]) {
+
+        }
+    private:
+
+    }
+}
---
+++ project2/src/main.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(int argc, char * argv[]) {
+
+    int N, R, E;
+
+    // argparser
+    if (argc == 4) {
+        
+    } else {
+        return 0;
+    }
+
+    std::cout << N << R << E;
+    return 0;
+}
