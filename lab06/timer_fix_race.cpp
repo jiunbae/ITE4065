7bbda7d979a12be37fd0e843e7722a063b45fea1
MaybeS <maybe@hanyang.ac.kr>
2017-10-19 21:22:33 +0900

    Update lab06
    Former-commit-id: 4f2dfdd9b97f6ebe461184f263de26312d66fd99

--- lab06/timer_fix_race.cpp
+++ lab06/timer_fix_race.cpp
@@ -3,6 +3,7 @@
 #include <boost/thread/thread.hpp>
 #include <boost/bind.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/thread/mutex.hpp>
 
 class Printer {
 public:
@@ -12,33 +13,34 @@ public:
           timer2_(io, boost::posix_time::seconds(1)),
           count_(0)
     {
-        timer1_.async_wait(boost::bind(&Printer::Print1, this));
-        timer2_.async_wait(boost::bind(&Printer::Print2, this));
+		boost::asio::io_service::strand strand(io);
+        timer1_.async_wait(strand.wrap(boost::bind(&Printer::Print1, this)));
+        timer2_.async_wait(strand.wrap(boost::bind(&Printer::Print2, this)));
     }
 
     ~Printer() {
         std::cout << "Final count is " << count_ << std::endl;
     }
     
-    void Print1() {
+    void Print1(boost::asio::io_service::strand* strand) {
         if (count_ < 10) {
             std::cout << "Timer 1: " << count_ << std::endl;
             ++count_;
 
             timer1_.expires_at(timer1_.expires_at()
                     + boost::posix_time::seconds(1));
-            timer1_.async_wait(boost::bind(&Printer::Print1, this));
+            timer1_.async_wait(strand->wrap(boost::bind(&Printer::Print1, this, strand)));
         }
     }
 
-    void Print2() {
+    void Print2(boost::asio::io_service::strand* strand) {
         if (count_ < 10) {
             std::cout << "Timer 2: " << count_ << std::endl;
             ++count_;
 
             timer2_.expires_at(timer2_.expires_at()
                     + boost::posix_time::seconds(1));
-            timer2_.async_wait(boost::bind(&Printer::Print2, this));
+            timer2_.async_wait(strand->wrap(boost::bind(&Printer::Print2, this, strand)));
         }
     }
 
@@ -51,6 +53,7 @@ private:
 
 int main(void) {
     boost::asio::io_service io;
+	boost::asio::io_service::strand strand(io);
     Printer p(io);
     boost::thread t(boost::bind(&boost::asio::io_service::run, &io));
     io.run();
