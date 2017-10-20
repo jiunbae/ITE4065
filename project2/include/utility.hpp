1a11240a8987397150a3720031fb01467f628bff
bae jiun, maybe <alice.maydev@gmail.com>
2017-10-20 14:41:16 +0900

    Back cuz, support of standard 17
    Former-commit-id: c6dfdfc7c0f10b693d56842c62a30cd64760d051

--- project2/include/utility.hpp
+++ project2/include/utility.hpp
@@ -47,27 +47,32 @@ namespace util {
                 // @see also: http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0305r0.html
                 // if (T v = next(); ret.find(v) == ret.end())
                 //     ret.insert(v)
-                   
             }
 
-			return next(ret, std::make_index_sequence<N>{});
+            std::vector<T> v(ret.begin(), ret.end());
+			return tuple_from_vector(v, std::make_index_sequence<N>());
 		}
 
     private:
         std::mt19937 gen;
         std::uniform_int_distribution<T> dis;
 
-		template <size_t... Is>
-		auto next(const std::unordered_set<T>& c, std::index_sequence<Is...>) {
-            // Imp: C++14 feature! but not on gcc
-            // Generic lambda-capture initializers
-            // @see also: http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2013/n3610.html
-            // return std::make_tuple([&b = c.begin()](size_t v) -> T {
-            auto b = c.begin();
-			return std::make_tuple([&b](size_t v) -> T {
-				std::advance(b, v);
-				return *b;
-			}(Is)...);
+        // Imp: C++14 feature!
+        // Generic lambda-capture initializers
+        // @see also: http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2013/n3610.html
+        // template <size_t... Indices>
+        // auto tuple_from_set(const std::unordered_set<T>& s, std::index_sequence<Indices...>) {
+        //     return std::make_tuple([&b = c.begin()](size_t v) -> T {
+        //         std::advance(b, v);
+        //         return *(b);
+        //     } (Indices)...);
+        // }
+        // According to the standard, It must work but not on gcc, cuz const reference capture problem
+        // It can reduce create temporary vector for make tuple from elements in runtime.
+
+		template <size_t... Indices>
+		auto tuple_from_vector(const std::vector<T>& v, std::index_sequence<Indices...>) {
+            return std::make_tuple(v[Indices]...);
 		}
     };
 }
