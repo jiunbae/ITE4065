11b80d9ec3b6529d1437a830d50131520a9d65ec
bae jiun, maybe <alice.maydev@gmail.com>
2017-10-27 22:38:28 +0900

    Update drawer
    Former-commit-id: 8d68b0a6fd066e28307e39ca0b1f8b54d278ee98

--- project3/draw.py
+++ project3/draw.py
@@ -1,27 +1,42 @@
-from subprocess import Popen, PIPE
+#!/bin/python
+'''
+    File name: draw.py
+    Author: Bae Jiun, Maybe
+    Date created: 10/27/2017
+    Requirements:
+        - matplotlib
+'''
 
+from subprocess import Popen, PIPE
 import matplotlib.pyplot as plt
 
 TARGET = "run"
-EXECUTE_PARAM = "./"
 RANGE = [1, 2, 4, 8, 16, 32]
 print ("Execute target file:", TARGET)
 print ("Execute range:", RANGE)
 
 def execute(count):
-    process = Popen([EXECUTE_PARAM + TARGET, str(count)], stdout=PIPE, stderr=PIPE)
+    def parse(out):
+        if ':' in out: return int(out.split(':')[1])
+        return int(out)
+
+    process = Popen(["./" + TARGET, str(count)], stdout=PIPE, stderr=PIPE)
     stdout, stderr = process.communicate()
     if stderr:
         raise Exception('Error while running {} {}'.format(TARGET, count), stderr)
-    return int(stdout[8:])
+    return parse(stdout)
 
 fig = plt.figure()
 for x, y in zip(RANGE, map(execute, RANGE)):
     print ("Result update count: {} in thread count: {}".format(y, x))
-    plt.scatter(x, y, s=10, color='BLACK')
+    plt.scatter(x, y, color='BLACK')
+
+# change y range if you need
+# plt.ylim((25,250))
 
 plt.xlabel("thread")
 plt.ylabel("update")
-plt.show()
+
 plt.savefig('testplot.png')
+plt.show()
 plt.close()
